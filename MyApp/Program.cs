6eed47c5440e968fc0c7751527d09fbd90bb240e
mbjn <mbjn@itu.dk>
2022-12-19 14:07:52 +0100

    Testing libgit2sharp

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,11 +1,14 @@
-﻿
+﻿using LibGit2Sharp;
+
 namespace MyApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            using (var repo = new Repository(@"C:\Users\Ejer\source\repos\ProjectBDSA")) {
+                Console.WriteLine(repo.Commits.Count());
+            }
         }
     }
 }
