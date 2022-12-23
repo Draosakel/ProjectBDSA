8be5a5c0ad2a252fe6344f3267443f18124368ab
mbjn <mbjn@itu.dk>
2022-12-23 13:02:56 +0100

    WebApi now allowes CORS Fixed fetchdata

--- BlazorWebAssemblyApp/Program.cs
+++ BlazorWebAssemblyApp/Program.cs
@@ -9,8 +9,3 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-builder.Services.AddScoped(sp => 
-    new HttpClient
-    {
-        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-    });
--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -1,10 +1,26 @@
-using Microsoft.EntityFrameworkCore;
-using LibGit2Sharp;
 using MyWebApi;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5258",
+                                              "http://localhost:5053")
+                                              .AllowAnyHeader()
+                                              .AllowAnyMethod()
+                                              ;
+                      });
+});
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapGet("/downloadrepos/{user}/{repo}", async (string user, string repo) =>
     {
