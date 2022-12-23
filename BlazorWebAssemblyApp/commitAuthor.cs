72a6fa770bcc00f0eb40d7e19fee0f0ed82af236
Christoffer H. Nielsen <hecn@itu.dk>
2022-12-23 11:53:52 +0100

    BlazorSheit

---
+++ BlazorWebAssemblyApp/commitAuthor.cs
@@ -0,0 +1,13 @@
+// namespace commitAuthor;
+
+// public class commitAuthor {
+//     public class Author {
+//         public string AuthorId { get; set; }
+//         public List<Commit> AuthorCommits { get; set; }
+//     }
+
+//     public class Commit {
+//         public int totalCommits { get; set; }
+//         public string commitDate { get; set; }
+//     }
+// }
